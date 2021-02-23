6339663087945aac4912e290a4c8faef24ddb8d4
ahmad amam <ahmadfatkhul.amam@gmail.com>
2021-02-24 00:46:39 +0700

    make fake profile REST API

--- Q5/listToDo/android/app/src/main/java/com/habdindalem/listdo/MainActivity.java
+++ Q5/listToDo/android/app/src/main/java/com/habdindalem/listdo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.listtodo;
+package com.habdindalem.listdo;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "listToDo";
+    return "List";
   }
 }
--- Q5/listToDo/ios/ListTests/ListTests.m
+++ Q5/listToDo/ios/ListTests/ListTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface listToDoTests : XCTestCase
+@interface ListTests : XCTestCase
 
 @end
 
-@implementation listToDoTests
+@implementation ListTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
