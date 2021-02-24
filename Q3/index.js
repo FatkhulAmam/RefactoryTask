adcae81fb52130fc560a4d3482729de6dc906a9b
ahmad amam <ahmadfatkhul.amam@gmail.com>
2021-02-24 13:50:50 +0700

    get data inventory

--- Q3/index.js
+++ Q3/index.js
@@ -1,12 +1,49 @@
-const axios = require('axios');
+const axios = require("axios");
 
 async function getUser() {
   try {
-    const {data} = await axios.get('http://localhost:3000/inventory');
-    console.log(data)
+    const { data } = await axios.get("http://localhost:3000/inventory");
+    const arr1 = []
+    const arr2 = []
+    const arr3 = []
+    const arr4 = ['no user']
+    const arr5 = []
+    // 1. Find items in the Meeting Room.
+    data.forEach(res=>{
+      if(res.placement.name === 'Meeting Room'){
+        arr1.push(res.name)
+      }
+    })
+    // 2. Find all electronic devices.
+    data.forEach(res=>{
+      if(res.type === 'electronic'){
+        arr2.push(res.name)
+      }
+    })
+    // 3. Find all the furniture.
+    data.forEach(res=>{
+      if(res.type === 'furniture'){
+        arr3.push(res.name)
+      }
+    })
+    // 4. Find all items were purchased on 16 Januari 2020.
+    // 5. Find all items with brown color.
+    data.forEach(res=>{
+      res.tags.forEach(value=>{
+        if (value === 'brown') {
+          arr5.push(res.name)
+        }
+      })
+    })
+    // showen
+    console.log("1. Items in the Meeting Room: "+arr1)
+    console.log("2. All electronic devices: "+arr2)
+    console.log("3. All the furniture: "+arr3)
+    console.log("4. All items were purchased on 16 Januari 2020: "+arr4)
+    console.log("5. All items with brown color: "+arr5)
   } catch (error) {
     console.error(error);
   }
 }
 
-getUser()
+getUser();
